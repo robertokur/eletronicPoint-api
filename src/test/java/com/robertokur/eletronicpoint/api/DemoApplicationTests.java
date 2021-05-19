0b635076d05b92740c9eeee92991b656a97e9868
robertokur <robertokur@gmail.com>
2021-05-19 19:02:14 -0300

    initial commit

---
+++ src/test/java/com/robertokur/eletronicpoint/api/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robertokur.eletronicpoint.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
